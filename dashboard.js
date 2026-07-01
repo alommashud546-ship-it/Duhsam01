fbbc030387ea5d8f8813e64dffd96bf48a4d9643
alommashud546-ship-it <alommashud546@gmail.com>
2026-07-02 00:01:25 +0530

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,24 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const balance = document.getElementById("balance");
 
 onAuthStateChanged(auth, (user) => {
+
   if (!user) {
     window.location.href = "login.html";
+    return;
   }
+
+  const userRef = doc(db, "users", user.uid);
+
+  onSnapshot(userRef, (docSnap) => {
+    if (docSnap.exists()) {
+      balance.innerText = "₹" + (docSnap.data().balance || 0);
+    } else {
+      balance.innerText = "₹0";
+    }
+  });
+
 });
