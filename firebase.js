88779a7fa4fd8741fe02ceb26f070288b7af868f
alommashud546-ship-it <alommashud546@gmail.com>
2026-07-01 12:45:13 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMeIK4RPrJxNBXsMoxDmEckpwM3JQkn0U",
-  authDomain: "duhsam.firebaseapp.com",
-  projectId: "duhsam",
-  storageBucket: "duhsam.firebasestorage.app",
-  messagingSenderId: "44011945600",
-  appId: "1:44011945600:web:193d27c735d0920d33b065"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
