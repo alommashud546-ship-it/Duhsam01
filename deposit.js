0225f1d0decc63189e5941569e84209a88a4fcbe
alommashud546-ship-it <alommashud546@gmail.com>
2026-07-01 12:07:50 +0530

    Add files via upload

--- deposit.js
+++ deposit.js
@@ -46,8 +46,9 @@ submit.addEventListener("click", async () => {
     utr.value = "";
     amount.value = "";
 
-  } catch (e) {
-    alert("Error: " + e.message);
-  }
+catch (e) {
+  alert("Error: " + e.message);
+  console.log(e);
+}
 
 });
